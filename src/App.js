b45ce3ba9af7847cd8c6158db7b7e2aa36973657
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-02-25 16:51:35 -0500

    iteration 1 through 2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 import Navbar from './components/Navbar';
 import countryArr from './countries.json';
 import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route
+          path="/:alpha3Code"
+          element={<CountryDetails countryList={countryArr} />}
+        />
+      </Routes>
       <CountriesList countries={countryArr} />
-      {/* <Routes>
-        <Route></Route>
-      </Routes> */}
     </div>
   );
 }
