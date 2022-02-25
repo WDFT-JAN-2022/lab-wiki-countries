efd355586903e67446a2117c0ebd6d9f27b7309f
Yeats Goodwin <yeatsg@gmail.com>
2022-02-25 17:03:36 -0500

    yeats iteration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import countryArr from './countries.json';
+// import countryArr from './countries.json';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countryArr, setCountryArr] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .request({
+        method: 'GET',
+        url: 'https://ih-countries-api.herokuapp.com/countries',
+      })
+      .then((results) => {
+        console.log('axios request results', results.data);
+        setCountryArr(results.data);
+      })
+      .catch((err) => {
+        console.log('Something went wrong', err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
